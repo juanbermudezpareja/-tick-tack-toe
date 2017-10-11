23844e8026011b717b321b9acc42573c925da801
ebejuan <juan.bermudez.pareja@ericsson.com>
2017-10-12 00:12:45 +0200

    Logic PC

--- src/js/main.js
+++ src/js/main.js
@@ -1,55 +1,70 @@
 var tablero = [[null, null, null], [null, null, null], [null, null, null]];
 
-// Jugador x
-//tablero[0][0] = 'X';
-// tablero[1][1] = 'X';
-// tablero[2][2] = 'X';
-
-// Jugador o
-// tablero[0][1] = 'O';
-// tablero[0][2] = 'O';
-// tablero[2][0] = 'O';
-
 leerTablero(tablero);
 
-function assignX(e,posX,poxY){
-    tablero[posX][poxY]='X';
-    leerTablero(tablero);   
-    e.target.innerText='X';
+function assignX(e, posX, poxY) {
+    tablero[posX][poxY] = 'X';
+    e.target.innerText = 'X';
+    if (leerTablero(tablero, 'X')===false){
+        juegaPC();
+    }
 }
 
-function leerTablero(tablero) {
-    if (tablero[0][0] == tablero[0][1] && tablero[0][1] == tablero[0][2] && tablero[0][0] != null) {
-        console.log("3 en raya para el jugador " + tablero[0][0]);
+function juegaPC() {
+    let x;
+    let y;
+    do {
+        x = getRandomInt(0,3);
+        y = getRandomInt(0,3);
+    } while (tablero[x][y] !== null)
+    
+    const divPC = 'div_' + x + y;
+    tablero[x][y] = 'O';
+    leerTablero(tablero, 'O');
+
+    document.getElementById(divPC).innerText = 'O';
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function leerTablero(tablero, jugador) {
+    if (tablero[0][0] != null && tablero[0][0] == tablero[0][1] && tablero[0][1] == tablero[0][2]) {
+        return displayInlineWiner(jugador);
+    } else if (tablero[0][0] != null && tablero[0][0] == tablero[1][0] && tablero[1][0] == tablero[2][0]) {
+        return displayInlineWiner(jugador);
+    } else if (tablero[0][1] != null && tablero[0][1] == tablero[1][1] && tablero[1][1] == tablero[2][1]) {
+        return displayInlineWiner(jugador);
+    } else if (tablero[0][2] != null && tablero[0][2] == tablero[1][2] && tablero[1][2] == tablero[2][2]) {
+        return displayInlineWiner(jugador);
+    } else if (tablero[0][0] != null && tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]) {
+        return displayInlineWiner(jugador);
+    } else if (tablero[0][2] != null && tablero[0][2] == tablero[1][2] && tablero[1][2] == tablero[2][0]) {
+        return displayInlineWiner(jugador);
+    } else if (tablero[1][0] != null && tablero[1][0] == tablero[1][1] && tablero[1][1] == tablero[1][2]) {
+        return displayInlineWiner(jugador);
+    } else if (tablero[2][0] != null && tablero[2][0] == tablero[2][1] && tablero[2][1] == tablero[2][2]) {
+        return displayInlineWiner(jugador);
     } else {
-        if (tablero[0][0] == tablero[1][0] && tablero[1][0] == tablero[2][0] && tablero[0][0] != null) {
-            console.log("3 en raya para el jugador " + tablero[0][0]);
-        } else {
-            if (tablero[0][1] == tablero[1][1] && tablero[1][1] == tablero[2][1] && tablero[0][1] != null) {
-                console.log("3 en raya para el jugador " + tablero[0][1]);
-            } else {
-                if (tablero[0][2] == tablero[1][2] && tablero[1][2] == tablero[2][2] && tablero[0][2] != null) {
-                    console.log("3 en raya para el jugador " + tablero[0][2]);
-                } else {
-                    if (tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2] && tablero[0][0] != null) {
-                        console.log("3 en raya para el jugador " + tablero[0][0]);
-                    } else {
-                        if (tablero[0][2] == tablero[1][2] && tablero[1][2] == tablero[2][0] && tablero[0][2] != null) {
-                            console.log("3 en raya para el jugador " + tablero[0][2]);
-                        } else {
-                            if (tablero[1][0] == tablero[1][1] && tablero[1][1] == tablero[1][2] && tablero[1][0] != null) {
-                                console.log("3 en raya para el jugador " + tablero[1][0]);
-                            } else {
-                                if (tablero[2][0] == tablero[2][1] && tablero[2][1] == tablero[2][2] && tablero[2][0] != null) {
-                                    console.log("3 en raya para el jugador " + tablero[2][0]);
-                                } else {
-                                    console.log("sigue jugando")
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
+        return false;
     }
+}
+
+function displayInlineWiner(player) {
+    const winer = 'win' + player;
+    document.getElementById(winer).style.display = 'inline';
+    console.log("3 en raya para el jugador " + player);
+}
+
+function anyBoxNotFilled(){
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            var element = tablero[i][j];
+            if (element === null){
+                return true;
+            }
+        }       
+    }
+    return false;
 }
